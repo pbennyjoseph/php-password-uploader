ebe9015c7e2218de4b76e3c4e17747b7e24be65a
Benny Joseph <jbenny37@gmail.com>
2019-09-06 21:07:36 +0530

    Fixed indentation and tags

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,12 +10,9 @@ session_start();
     <link rel="icon" href="favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-</head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <link href="signin.css" rel="stylesheet">
 </head>
 
@@ -36,7 +32,6 @@ session_start();
                         </label>
                         <p class='label label-info' id="upload-file-info"></p>
                         <p class='label label-info' id="retval"></p>
-                        <!-- <input class="btn btn-primary btn-block" type="file" name="fileToUpload" id="fileToUpload"> -->
                         <button name="submit" class="btn btn-lg btn-primary btn-block" type="submit">Verify and upload
                         </button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
